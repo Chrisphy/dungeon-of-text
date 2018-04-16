966721047009596d2f294bdd60fa6831791dd5e8
chris <chrischong1997@gmail.com>
2018-04-16 10:08:37 +1000

    Added main story activity and java class

--- app/src/main/java/deakin/dungeonoftext/HelpActivity.java
+++ app/src/main/java/deakin/dungeonoftext/HelpActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -14,10 +15,28 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        final Button backbtn = (Button) findViewById(R.id.back_button);
 
 
         ImageView img= (ImageView) findViewById(R.id.image);
         img.setImageResource(R.drawable.help_screen);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+
+
+                }
+                catch (Exception e){
+                    Log.e("Error", "Failed to go to Helpactivity");
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
 
     }
--- app/src/main/java/deakin/dungeonoftext/StoryActivity.java
+++ app/src/main/java/deakin/dungeonoftext/StoryActivity.java
@@ -1,15 +1,13 @@
 package deakin.dungeonoftext;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class StoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
